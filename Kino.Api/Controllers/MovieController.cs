2f88db2160dad229d21028b6543a95625d9074a0
Каримжонов Ойбек < @DESKTOP-CJT0MDD>
2023-10-15 22:17:01 +0300

    INFRA: Create Controller

---
+++ Kino.Api/Controllers/MovieController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kino.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class MovieController : Controller
+    {
+       
+    }
+}
